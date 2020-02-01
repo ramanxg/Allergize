aad1c5a1b21fbdacc2f368a87b79f0ae0995c281
Raman Gupta <ramanxg@gmail.com>
2020-02-01 08:40:18 -0800

    added firebase routes

--- server/app.js
+++ server/app.js
@@ -4,6 +4,24 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+// firebase configuration
+var firebase = require("firebase/app");
+require("firebase/auth");
+require("firebase/firestore");
+
+var firebaseConfig = {
+  apiKey: "AIzaSyAnA5BzfPyx1sVHCj7TgYVwVdXU33DCs2c",
+  authDomain: "nonut-52bd9.firebaseapp.com",
+  databaseURL: "https://nonut-52bd9.firebaseio.com",
+  projectId: "nonut-52bd9",
+  storageBucket: "nonut-52bd9.appspot.com",
+  messagingSenderId: "186976534326",
+  appId: "1:186976534326:web:2399c9f6e0afad009dfd97"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -31,7 +49,7 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send('error');
 });
 
 module.exports = app;
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,13 +1,107 @@
 var express = require('express');
 var router = express.Router();
 
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+var database = firebase.firestore();
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
+  
   res.json({name:"Aaron", password:"hehexd"});
 });
 
 router.get('/createUser', function(req, res, next) {
-  res.send(' Creating User!');
+  console.log("creating a user")
+  data = {
+    name: 'Raman Gupta',
+    email: 'ramanxg@gmail.com',
+    password: 'helloworld'
+  }
+  // data = {
+  //   name: req.body.name,
+  //   email: req.body.email,
+  //   password: req.body.password
+  // }
+  //email = req.body.email;    password = req.body.password
+  firebase.auth().createUserWithEmailAndPassword(data.email, data.password).then(function(user) {
+    let user_data = {
+      name: data.name,
+      email: data.email,
+      allergy_list: []
+    }
+    let setDoc = database.collection('users').doc(data.email).set(user_data);
+    res.json({'successful': true});
+  }).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    if (errorCode === 'auth/email-already-in-use') {
+      console.log("Email is already being used");
+    } else {
+      console.log(error.message);
+    }
+    res.json({'successful': false, 'message': 'Email is already being used'});
+  });
+  // if (success) {res.send('Success')} else {res.send('Failure')}
 });
+
+router.get('/loginUser', function(req, res, next) {
+  data = {
+    email: 'ramanxg@gmail.com',
+    password: 'helloworld'
+  }
+  //email = req.body.email;    password = req.body.password
+  firebase.auth().signInWithEmailAndPassword(data.email, data.password)
+    .then(function(firebaseUser) {
+      //success
+      res.json({'successful': true});
+    })
+    .catch(function(error) {
+      // Handle Errors here.
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      if (errorCode === 'auth/user-not-found') {
+        res.json({'successful': false, 'message': 'Email not found'});
+      }
+      else if (errorCode === 'auth/wrong-password') {
+        res.json({'successful': false, 'message': 'Incorrect Password'});
+      } else {
+        console.log(errorMessage);
+      }
+      console.log(error);
+  });
+});
+
+
+
+router.get('/addAllergy', function(req, res, next) {
+  data = {email: 'ramanxg@gmail.com', new_allergies: ['milk', 'wheat']}
+  // data = {new_allergies: req.body.new_allergies}
+  var user = firebase.auth().currentUser;
+  if (user) {
+    let userRef = database.collection('users').doc(user.email);
+    userRef.get().then( function(docSnap) {
+      let arrUnion = userRef.update({
+        allergy_list: firebase.firestore.FieldValue.arrayUnion(...data.new_allergies)
+      }).then(function(p) {
+        res.json({successful:true})
+      }).catch((err) => {
+        console.log("Error: ", err);
+        res.json({successful:false, "message": err})
+      });
+    });
+    
+  } else {
+    console.log("User not logged in.")
+    res.json({successful:false, "message": "User not logged in."})
+  }
+  
+});
+
 
 module.exports = router;
