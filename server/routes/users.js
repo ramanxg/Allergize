f1282305c5729b4dec0c2664dc65670e9685c461
Raman Gupta <ramanxg@gmail.com>
2020-01-31 23:12:15 -0800

    nodemon

--- server/routes/users.js
+++ server/routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hi From Users!');
+  res.json({name:"Aaron", password:"hehexd"});
+});
+
+router.get('/createUser', function(req, res, next) {
+  res.send(' Creating User!');
 });
 
 module.exports = router;
