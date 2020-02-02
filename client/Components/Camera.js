3b95d69a6f4c8d30b2c363af78636c1896e3ce1c
Brian Wang <bwang@dhcp-v109-019.mobile.uci.edu>
2020-02-02 02:12:02 -0800

    Again

--- client/Components/Camera.js
+++ client/Components/Camera.js
@@ -78,7 +78,6 @@ export default class Capture extends React.Component {
       console.log("pictureTaken:" + pictureTaken);
       return (
         <View style={{ flex: 1}}>
-<<<<<<< HEAD
             {pictureTaken && <ImageBackground style = {{flex: 1}} source={{uri: `data:image/gif;base64,${picture}`}}></ImageBackground>}
             {!pictureTaken && <><Camera style={{ flex: 1 }} type={this.state.cameraType} ref={ref => {this.camera = ref;}}></Camera>
             <View>
@@ -93,18 +92,6 @@ export default class Capture extends React.Component {
                     />  
                 </TouchableOpacity>
             </View></>}
-=======
-            <Camera style={{ flex: 1 }} type={this.state.cameraType} ref={ref => {this.camera = ref;}}></Camera>
-            <View style={{flex: 0.1, backgroundColor: 'black', alignItems: 'center', justifyContent: 'center'}}>
-              {pictureTaken && <Text style={{fontSize: 20, color: 'white',}}>Processing Image...</Text>}
-              {!pictureTaken && <TouchableOpacity style={styles.takePictureButton} onPress={this.takePicture}>
-                  <Icon
-                      type='font-awesome'
-                      name='camera'
-                  />  
-              </TouchableOpacity>}
-            </View>
->>>>>>> fcd4b3e28b2166ca14f13684fd84a5e1186945af
         </View>
       );
     }
